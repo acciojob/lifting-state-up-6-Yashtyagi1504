ec033e59d94c29113bee8bc3fc7480e7c07c6391
Yashtyagi1504 <yashtyagi1552@gmail.com>
2025-04-09 01:55:24 +0530

    Fourth

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,34 +1,65 @@
-import React from "react";
- function TodoList({todos,handleComplete}){
+// import React from "react";
+//  function TodoList({todos,handleComplete}){
 
-    function handleClick(e){
-        let index = parseInt(e.target.id);
-        let newArr = todos.map((value,i)=>
-            index == i ? {...value,status:false}:value
-        )
-          handleComplete(newArr)
+//     function handleClick(e){
+//         let index = parseInt(e.target.id);
+//         let newArr = todos.map((value,i)=>
+//             index == i ? {...value,status:false}:value
+//         )
+//           handleComplete(newArr)
         
-    }
+//     }
 
-    return(
-        <div>
-            <h2>Child Component</h2>
-            <ul>
-                {
-                    todos.map((str,index) =>(
-                        <div>
-                        <li>{str.text}
-                            {str.status?<button onClick={handleClick} id={index}>Complete</button>:null}
-                        </li>
+//     return(
+//         <div>
+//             <h2>Child Component</h2>
+//             <ul>
+//                 {
+//                     todos.map((str,index) =>(
+//                         <div>
+//                         <li>{str.text}
+//                             {str.status?<button onClick={handleClick} id={index}>Complete</button>:null}
+//                         </li>
                         
-                        </div>
-                    ))
-                }
-            </ul>
-        </div>
-    )
+//                         </div>
+//                     ))
+//                 }
+//             </ul>
+//         </div>
+//     )
 
 
- }
+//  }
 
- export default TodoList;
+//  export default TodoList;
+
+import React from "react";
+
+function TodoList({ todos, handleComplete }) {
+  function handleClick(index) {
+    const updatedTodos = todos.map((todo, i) =>
+      i === index ? { ...todo, status: false } : todo
+    );
+    handleComplete(updatedTodos);
+  }
+
+  return (
+    <div>
+      <h2>Child Component</h2>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo.text}
+            {todo.status && (
+              <button onClick={() => handleClick(index)} id={`btn-${index}`}>
+                Complete
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
