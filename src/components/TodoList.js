8b63f2f9f225f51afa4adc05a42e07fa00bb291f
Yashtyagi1504 <yashtyagi1552@gmail.com>
2025-04-09 01:50:49 +0530

    Third

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,7 @@ import React from "react";
  function TodoList({todos,handleComplete}){
 
     function handleClick(e){
-        let index = parseInt(e.target.id) - 1;
+        let index = parseInt(e.target.id);
         let newArr = todos.map((value,i)=>
             index == i ? {...value,status:false}:value
         )
@@ -18,7 +18,7 @@ import React from "react";
                     todos.map((str,index) =>(
                         <div>
                         <li>{str.text}
-                            {str.status?<button onClick={handleClick} id={index+1}>Complete</button>:null}
+                            {str.status?<button onClick={handleClick} id={index}>Complete</button>:null}
                         </li>
                         
                         </div>
